07a447a8223e1f9a1c12ae16d9e67d3521da2b96
Amine Benhebba <amine.benhebba.ing@gmail.com>
2022-07-21 00:53:08 +0100

    Add Raid Reference To RaidSetup Entity.

--- src/Raider.Domain/Entities/RaidSetup.cs
+++ src/Raider.Domain/Entities/RaidSetup.cs
@@ -5,7 +5,10 @@ namespace Raider.Domain.Entities
     public class RaidSetup
     {
         public Guid Id { get; set; }
+        public Guid RaidId { get; set; }
         public string Name { get; set; }
         public string TemplatePath { get; set; }
+
+        public Raid Raid { get; set; }
     }
 }
