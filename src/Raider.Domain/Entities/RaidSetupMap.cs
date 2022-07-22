994324fb71a08e308acf99278309a2c8b16b24f5
Amine Benhebba <amine.benhebba.ing@gmail.com>
2022-07-23 00:03:16 +0100

    Add RaidSetupMap To Save The Template On Database.

---
+++ src/Raider.Domain/Entities/RaidSetupMap.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Raider.Domain.Entities
+{
+    public class RaidSetupMap
+    {
+        public Guid RaidSetupId { get; set; }
+        public string SpecialisationId { get; set; }
+        public int Group { get; set; }
+        public int Index { get; set; }
+
+        public RaidSetup RaidSetup { get; set; }
+        public Specialisation Specialisation { get; set; }
+    }
+}
