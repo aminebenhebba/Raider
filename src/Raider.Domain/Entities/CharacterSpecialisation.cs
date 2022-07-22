e207c877d34054528c287296bc715b696e83fb98
Amine Benhebba <amine.benhebba.ing@gmail.com>
2022-07-22 05:13:56 +0100

    Add Character specialisations And Gear Score.

---
+++ src/Raider.Domain/Entities/CharacterSpecialisation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Raider.Domain.Entities
+{
+    public class CharacterSpecialisation
+    {
+        public Guid CharacterId { get; set; }
+        public string SpecialisationId { get; set; }
+        public string? GearScore { get; set; }
+
+        public Character Character { get; set; }
+        public Specialisation Specialisation { get; set; }
+    }
+}
