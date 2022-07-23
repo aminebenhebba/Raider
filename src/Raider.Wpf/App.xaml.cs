c2f87d56f9990c31f3f8427a4823e0c5a3c51119
Amine Benhebba <amine.benhebba.ing@gmail.com>
2022-07-23 21:28:09 +0100

    Add Dependency Injection To Raider.wpf
    Add D.I system to the project and wire it up for the
    MainWindow.

--- src/Raider.Wpf/App.xaml.cs
+++ src/Raider.Wpf/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -8,17 +10,35 @@ using System.Windows;
 
 namespace Raider.Wpf
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-        protected override void OnStartup(StartupEventArgs e)
+        public static IHost AppHost { get; private set; }
+
+        public App()
         {
-            MainWindow = new MainWindow();
+            AppHost = Host.CreateDefaultBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddSingleton<MainWindow>();
+                })
+                .Build();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await AppHost.StartAsync();
+
+            MainWindow = AppHost.Services.GetRequiredService<MainWindow>();
             MainWindow.Show();
 
             base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            await AppHost.StopAsync();
+
+            base.OnExit(e);
         }
     }
 }
